5b9070ce46d0f2c8dd7e1733e5c4b0bc9f99e60f
Gopesh Sharma <gopesh.sharma@gmail.com>
2023-08-14 02:03:07 -0400

    reconfigured

--- src/main/java/tech/luv2code/sms/StudentManagementSystemApplication.java
+++ src/main/java/tech/luv2code/sms/StudentManagementSystemApplication.java
@@ -11,26 +11,25 @@ import tech.luv2code.sms.repository.StudentRepository;
 @SpringBootApplication
 public class StudentManagementSystemApplication implements CommandLineRunner {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 		System.out.println("Hello Students!! ");
 	}
-	
+
 	@Autowired
 	private StudentRepository studentRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		// insert some students
-		/*
-		 * Student student1 = new Student("Guruji", "Bhagwan",
-		 * "gurujipuri@hotmail.com"); Student student2 = new Student("Vaishali",
-		 * "Sharma", "vaishali@gmail.com"); Student student3 = new Student("Kirti",
-		 * "Sharma", "kirti@gmail.com"); studentRepository.save(student1);
-		 * studentRepository.save(student2); studentRepository.save(student3);
-		 */
-		
+
+		Student student1 = new Student("Guruji", "Bhagwan", "gurujipuri@hotmail.com");
+		Student student2 = new Student("Vaishali", "Sharma", "vaishali@gmail.com");
+		Student student3 = new Student("Kirti", "Sharma", "kirti@gmail.com");
+		studentRepository.save(student1);
+		studentRepository.save(student2);
+		studentRepository.save(student3);
+
 	}
 
 }
